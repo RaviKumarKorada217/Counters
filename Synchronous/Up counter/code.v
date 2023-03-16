3e8fd9986154bcc2885143b0cf8747f312e3eef8
Ravi Kumar <124238054+RaviKumarKorada217@users.noreply.github.com>
2023-03-16 16:01:41 +0530

    Create code.v

---
+++ Synchronous/Up counter/code.v
@@ -0,0 +1,16 @@
+`timescale 1ns / 1ps
+
+module sync_up_counter( clk, rst, count);   
+    parameter N=4;    
+    input clk, rst;
+    output reg [N-1:0] count ;
+    
+	always@(posedge clk)
+	begin
+	   if(rst)
+	       count <= 0;
+	   else
+	       count <= count+1;
+	end
+
+endmodule
