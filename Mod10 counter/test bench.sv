519f4858ce29d05951faccf0cea54659bac68c2f
Ravi Kumar <124238054+RaviKumarKorada217@users.noreply.github.com>
2023-03-16 16:48:33 +0530

    Create test bench.sv

---
+++ Mod10 counter/test bench.sv
@@ -0,0 +1,35 @@
+`timescale 1ns / 1ps
+
+
+module mod10_counter_test;
+
+	// Inputs
+	reg clk;
+	reg rst;
+
+	// Outputs
+	wire [3:0] count;
+
+	// Instantiate the Unit Under Test (UUT)
+	phone_number_counter uut (
+		.clk(clk), 
+		.rst(rst),
+		.count(count)
+	);
+	always #5 clk = ~clk;
+	initial begin
+		rst = 1;
+		clk = 0;
+		
+		#100;
+		rst = 0;
+	end
+	
+	initial begin
+	   $dumpfile("dump.vcd");
+	   $dumpvars;
+	   
+	   #2000; $finish();
+	end
+      
+endmodule
